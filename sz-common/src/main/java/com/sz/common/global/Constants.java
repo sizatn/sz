128a0dbcc00f9390235126cdfeb6507c20d6bfa3
sizatn <sizatn2013@gmail.com>
2018-05-05 15:47:20 +0800

    add modules

---
+++ sz-common/src/main/java/com/sz/common/global/Constants.java
@@ -0,0 +1,14 @@
+package com.sz.common.global;
+
+/**
+ * 
+ * @desc 通用常量类, 单个模块或系统的常量请新建一个package，并以模块或系统命名, 方便常量的分类管理
+ * @author sizatn
+ * @date May 5, 2018
+ */
+public class Constants {
+
+	public static final String SUCCESS_CODE = "100";
+	public static final String SUCCESS_MSG = "请求成功";
+
+}
