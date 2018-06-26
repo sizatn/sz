f5cea5782a1b69c22f8dc43d25465ebedfecbcba
sizatn <sizatn2013@gmail.com>
2018-06-26 12:07:30 +0800

    update

--- sz-utils/src/main/java/com/sizatn/sz/utils/common/ObjectUtil.java
+++ sz-utils/src/main/java/com/sizatn/sz/utils/common/ObjectUtil.java
@@ -2,6 +2,7 @@ package com.sizatn.sz.utils.common;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Method;
@@ -43,41 +44,72 @@ public class ObjectUtil extends org.apache.commons.lang3.ObjectUtils {
 	}
 	
 	/**
-	 * 序列化对象
 	 * @param object
 	 * @return
+	 * @desc 序列化
+	 * @author sizatn
+	 * @date Jun 26, 2018
 	 */
 	public static byte[] serialize(Object object) {
 		ObjectOutputStream oos = null;
 		ByteArrayOutputStream baos = null;
 		try {
-			if (object != null) {
-				baos = new ByteArrayOutputStream();
-				oos = new ObjectOutputStream(baos);
-				oos.writeObject(object);
-				return baos.toByteArray();
-			}
+			baos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(object);
+			return baos.toByteArray();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			if (baos != null) {
+				try {
+					baos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (oos != null) {
+				try {
+					oos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return null;
 	}
 
 	/**
-	 * 反序列化对象
 	 * @param bytes
 	 * @return
+	 * @desc 反序列化
+	 * @author sizatn
+	 * @date Jun 26, 2018
 	 */
-	public static Object unserialize(byte[] bytes) {
+	public static Object unSerialize(byte[] bytes) {
 		ByteArrayInputStream bais = null;
+		ObjectInputStream ois = null;
 		try {
-			if (bytes != null && bytes.length > 0) {
-				bais = new ByteArrayInputStream(bytes);
-				ObjectInputStream ois = new ObjectInputStream(bais);
-				return ois.readObject();
-			}
+			bais = new ByteArrayInputStream(bytes);
+			ois = new ObjectInputStream(bais);
+			return ois.readObject();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			if (bais != null) {
+				try {
+					bais.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (ois != null) {
+				try {
+					ois.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return null;
 	}
