3db1e73c65e8d9faea44d459f8f89d516a296303
sizatn <sizatn2013@gmail.com>
2018-09-13 18:54:33 +0800

    update

--- sz-webapp/src/main/java/com/sizatn/sz/webapp/configuration/DruidMonitorConfig.java
+++ sz-webapp/src/main/java/com/sizatn/sz/webapp/configuration/DruidMonitorConfig.java
@@ -15,9 +15,8 @@ public class DruidMonitorConfig {
 	 * 注册ServletRegistrationBean
 	 */
 	@Bean
-	public ServletRegistrationBean<StatViewServlet> registrationBean() {
-		ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<StatViewServlet>(
-				new StatViewServlet(), "/druid/*");
+	public ServletRegistrationBean<StatViewServlet> druidStatViewServlet() {
+		ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<StatViewServlet>(new StatViewServlet(), "/druid/*");
 
 		/** 初始化参数配置，initParams **/
 		// 登录查看信息的账号密码.
@@ -25,8 +24,7 @@ public class DruidMonitorConfig {
 		bean.addInitParameter("loginPassword", "admin");
 		// IP白名单 (没有配置或者为空，则允许所有访问)
 		bean.addInitParameter("allow", "");
-		// IP黑名单 (共存时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view
-		// this page.
+		// IP黑名单 (共存时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
 		bean.addInitParameter("deny", "192.88.88.88");
 		// 禁用HTML页面上的“Reset All”功能
 		bean.addInitParameter("resetEnable", "false");
@@ -38,7 +36,7 @@ public class DruidMonitorConfig {
 	 * 注册FilterRegistrationBean
 	 */
 	@Bean
-	public FilterRegistrationBean<WebStatFilter> druidStatFilter() {
+	public FilterRegistrationBean<WebStatFilter> druidWebStatFilter() {
 		FilterRegistrationBean<WebStatFilter> bean = new FilterRegistrationBean<WebStatFilter>(new WebStatFilter());
 		// 添加过滤规则.
 		bean.addUrlPatterns("/*");
